08f223a649cfea3b28680859b9739285f041f574
Ruya2001 <109046712+Ruya2001@users.noreply.github.com>
2023-11-09 16:59:23 +0300

    Add files via upload

---
+++ Default.aspx
@@ -0,0 +1,12 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="KFMS_PROJECT._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+        <div class="s flex flex-col items-center justify-center h-screen" >
+            <div class="text-center mt-8">
+
+                <a runat="server" href="~/src/login" class="btn btn-primary btn-lg " style="background:#38a6aa">Log In</a>
+                <a runat="server" href="~/src/signup" class="btn btn-secondary btn-lg">Sign UP</a>
+
+            </div>
+        </div>
+</asp:Content>
---
+++ Global.asax.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+using System.Web.Routing;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace KFMS_PROJECT
+{
+    public class Global : HttpApplication
+    {
+        void Application_Start(object sender, EventArgs e)
+        {
+            // Code that runs on application startup
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+        protected void Session_Start(object sender, EventArgs e)
+        {
+            // Check if UserID is not set in session
+            if (Session["UserID"] == null)
+            {
+                // Redirect to the login page
+                Response.Redirect("~/default.aspx"); // Change the URL as needed
+            }
+        }
+    }
+}
